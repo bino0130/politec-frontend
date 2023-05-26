2d57b002c6580dee7db9cf79d11fe5f27987217e
bino <ska3287776@naver.com>
2023-05-26 16:28:09 +0900

    Create StudentItemDaoTest.java

---
+++ 기업업무 코드/JunitTest/src/test/java/kr/ac/kopo/ctc/kopo10/dao/StudentItemDaoTest.java
@@ -0,0 +1,61 @@
+package kr.ac.kopo.ctc.kopo10.dao;
+
+//import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import kr.ac.kopo.ctc.kopo10.domain.StudentItem;
+
+class StudentItemDaoTest {
+
+	@Test
+	void test_createTable() {
+		StudentItemDao studentItemDao = new StudentItemDaoImpl();
+		StudentItem studentItem = studentItemDao.selectOne(5);
+		
+		studentItemDao.createTable();
+	}
+	
+	@Test
+	void test_drop() {
+		StudentItemDao studentItemDao = new StudentItemDaoImpl();
+		StudentItem studentItem = studentItemDao.selectOne(5);
+		
+		studentItemDao.dropTable();
+	}
+	
+	@Test
+	void test_create() {
+		StudentItemDao studentItemDao = new StudentItemDaoImpl();
+		StudentItem studentItem = studentItemDao.selectOne(5);
+		
+		studentItemDao.create();
+	}
+	
+	@Test
+	void test_select_one(){
+		StudentItemDao studentItemDao = new StudentItemDaoImpl();
+		StudentItem studentItem = studentItemDao.selectOne(5);
+		
+		assertEquals(studentItem.getName(), "홍길5");
+		assertEquals(studentItem.getStudentid(),5);
+		assertEquals(studentItem.getKor(),20);
+		assertEquals(studentItem.getEng(),40);
+		assertEquals(studentItem.getMat(),80);
+	}
+	
+	@Test
+	void test_select_All() {
+		StudentItemDao studentItemDao = new StudentItemDaoImpl();
+		List<StudentItem> sla = studentItemDao.selectAll(2, 5);
+		StudentItem selAll = sla.get(0);
+		
+		assertEquals(selAll.getName(),"홍길6");
+	}
+}
